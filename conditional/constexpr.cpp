b17e62db27925e3690a0480b932fc0fc7889df59
Tamás Morcsányi <mtomi8@gmail.com>
2024-09-18 12:15:23 +0000

    add vscode settings

--- conditional/constexpr.cpp
+++ conditional/constexpr.cpp
@@ -7,6 +7,18 @@ int main()
     const int number = -5;
 
     // TODO: Use if constexpr to check if the number is positive, negative or zero.
+    if constexpr (number > 0)
+    {
+        cout << number << " positive number." << endl;
+    }
+    else if constexpr (number < 0)
+    {
+        cout << number << " negativ number." << endl;
+    }
+    else
+    {
+        cout << "Number is 0." << endl;
+    }
 
     return 0;
 }
